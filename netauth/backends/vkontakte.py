f81aeb92d097fdce29d5ee8e22136d3d8bf30126
Kirill Klenov <horneds@gmail.com>
2010-11-25 02:54:16 +0300

    test

--- netauth/backends/vkontakte.py
+++ netauth/backends/vkontakte.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 
 from netauth.backends import OAuthBaseBackend
-from netauth import log
 
 
 try:
@@ -26,24 +25,7 @@ class VkontakteBackend(OAuthBaseBackend):
         else:
             self.error(request)
 
-        # cookie_name = "vk_app_%s" % settings.VKONTAKTE_APPLICATION_ID
-        # try:
-            # cookie_data = self.parse_qs(request.COOKIES[cookie_name])
-            # value = ""
-            # for i in ('expire', 'mid', 'secret', 'sid'):
-                # value += "%s=%s" % (i, cookie_data[i][0] )
-            # if cookie_data['sig'][0] == md5(value + settings.VKONTAKTE_APPLICATION_SECRET).hexdigest():
-                # self.identity = cookie_data['mid'][0]
-            # else:
-                # raise ValueError()
-        # except (KeyError, IndexError, AttributeError, ValueError):
-            # log.info(value)
-            # log.info(value + settings.VKONTAKTE_APPLICATION_SECRET)
-            # log.info(cookie_data['sig'][0])
-            # log.info(md5(value + settings.VKONTAKTE_APPLICATION_SECRET))
-            # log.info(md5(value + settings.VKONTAKTE_APPLICATION_SECRET).hexdigest())
-            # self.error(request)
         return data
 
     def get_extra_data(self, response):
-        return self.parse_qs(response)
+        return response
