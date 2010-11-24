ebeacf4204447030662f88f63e40c56e98694325
Kirill Klenov <horneds@gmail.com>
2010-11-25 01:44:07 +0300

    fix vkontakte

--- netauth/backends/vkontakte.py
+++ netauth/backends/vkontakte.py
@@ -1,8 +1,6 @@
-import urlparse
-
 from django.conf import settings
 
-from netauth.backends import BaseBackend
+from netauth.backends import OAuthBaseBackend
 
 
 try:
@@ -12,22 +10,23 @@ except ImportError:
     md5 = md5.new
 
 
-
-
-
-class VkontakteBackend(BaseBackend):
+class VkontakteBackend(OAuthBaseBackend):
 
     def validate(self, request, data):
-        cookie_name = "vk_app_%s" % settings.VKONTAKTE_APP_ID
+        cookie_name = "vk_app_%s" % settings.VKONTAKTE_APPLICATION_ID
         try:
-            cookie_data = urlparse.parse_qs(request.COOKIES[cookie_name])
+            content = request.COOKIES[cookie_name]
+            cookie_data = self.parse_qs(content)
             value = ""
             for i in ('expire', 'mid', 'secret', 'sid'):
                 value += "%s=%s" % (i, cookie_data[i][0] )
-            if cookie_data['sig'][0] == md5(value + settings.VKONTAKTE_SECRET_KEY).hexdigest():
+            if cookie_data['sig'][0] == md5(value + settings.VKONTAKTE_APPLICATION_SECRET).hexdigest():
                 self.identity = cookie_data['mid'][0]
             else:
                 raise ValueError()
         except (KeyError, IndexError, AttributeError, ValueError):
             self.error(request)
+        return content
 
+    def get_extra_data(self, response):
+        return self.parse_qs(response)
