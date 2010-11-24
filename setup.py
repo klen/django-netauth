526cba54190269573ffbb3bf145463dc9e554c8d
Kirill Klenov <horneds@gmail.com>
2010-11-24 16:25:43 +0300

    Fix setup

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ from setuptools import setup, find_packages
 from publicauth import VERSION, PROJECT
 
 
-BASE_DIR = os.path.abspath(os.path.join( os.path.dirname( __file__ ), 'publicauth', 'templates'))
+MODULE_NAME = 'publicauth'
+BASE_DIR = os.path.abspath(os.path.join( os.path.dirname( __file__ ), MODULE_NAME, 'templates'))
 PACKAGE_DATA = list()
 
 for root, dirs, files in os.walk( BASE_DIR ):
     for filename in files:
-        PACKAGE_DATA.append("%s/%s" % ( root[8:], filename ))
+        PACKAGE_DATA.append("%s/%s" % ( root[len(MODULE_NAME):], filename ))
 
 
 setup(
