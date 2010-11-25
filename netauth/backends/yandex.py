c88d3946e224f3a05b82196d1f4e2c846bf0f931
Kirill Klenov <horneds@gmail.com>
2010-11-25 14:28:40 +0300

    test

--- netauth/backends/yandex.py
+++ netauth/backends/yandex.py
@@ -7,7 +7,7 @@ class YandexBackend( OAuthBaseBackend ):
     APPLICATION_ID = property(lambda self: getattr(settings, "%s_APPLICATION_ID" % self.provider.upper()))
 
     def begin( self, request, data ):
-        self.get_request( url= self.AUTHORIZE_URL , parameters = { 'client_id': self.APPLICATION_ID, 'response_type': 'token' })
+        request = self.get_request( url= self.AUTHORIZE_URL , parameters = { 'client_id': self.APPLICATION_ID, 'response_type': 'token' })
         raise Redirect(request.to_url())
 
     def validate( self, request, data ):
